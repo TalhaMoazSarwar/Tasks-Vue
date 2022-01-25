b3f1c44a51dfc4d492405693c190fa51a56043df
Talha Moaz Sarwar <talha@taaruff.com>
2022-01-25 22:36:23 +0500

    Add task form validation added

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -7,6 +7,10 @@
       class="border p-1"
       v-model="name"
     />
+
+    <p class="text-sm text-red-600" v-if="(message = hasError('name'))">
+      {{ message }}
+    </p>
   </div>
   <div class="flex flex-col mt-5">
     <label class="text-sm text-gray-600">Day & Time</label>
@@ -16,6 +20,9 @@
       class="border p-1"
       v-model="time"
     />
+    <p class="text-sm text-red-600" v-if="(message = hasError('time'))">
+      {{ message }}
+    </p>
   </div>
   <button class="bg-black text-white w-full mt-3 py-1" @click="addTask">
     Add Task
@@ -29,19 +36,49 @@ export default {
       index: 0,
       name: "",
       time: "",
+      errors: [],
     };
   },
   methods: {
     addTask() {
-      let data = {
-        id: this.index++,
-        text: this.name,
-        date: this.time,
-        active: true,
-      };
-      this.name = "";
-      this.time = "";
-      this.$emit("addTask", data);
+      if (this.formValidated()) {
+        let data = {
+          id: this.index++,
+          text: this.name,
+          date: this.time,
+          active: true,
+        };
+
+        this.$emit("addTask", data);
+
+        this.name = "";
+        this.time = "";
+      }
+    },
+    formValidated() {
+      // validation
+      this.errors = [];
+      if (!this.name) {
+        this.errors.push({
+          name: "name",
+          message: "This task name field is required!",
+        });
+      }
+
+      if (!this.time) {
+        this.errors.push({
+          name: "time",
+          message: "Please select day and time of the task!",
+        });
+      }
+
+      if (!this.errors.length) {
+        return true;
+      }
+    },
+    hasError(v) {
+      let error = this.errors.find((e) => e.name === v);
+      return error ? error.message : false;
     },
   },
   emits: ["addTask"],
